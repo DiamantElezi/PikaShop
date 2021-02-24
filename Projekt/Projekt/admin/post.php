66e22a83956f339ab6a7e2a532d217451156549a
DiamantElezi <elezidiamant@gmail.com>
2021-02-24 01:48:13 +0100

    Php dashboard posts

--- Projekt/Projekt/admin/post.php
+++ Projekt/Projekt/admin/post.php
@@ -3,23 +3,20 @@ require 'database/dbconfig.php';
 include 'inc/head.php';
 include 'inc/navbar.php';
 
-$products = $connection -> query("SELECT * FROM products")->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 
-<div class="product-center container">
-      <div class="product">
-        <div class="img-cover">
-          <img src="./img/<?php echo $row ["image"]; ?>" alt="" />
-        </div>
-        <p><?php echo $row ["title"] ?></p>
-        <div class="rating">
-          <i class="bx bxs-star"></i>
-          <i class="bx bxs-star"></i>
-          <i class="bx bxs-star"></i>
-          <i class="bx bxs-star"></i>
-          <i class="bx bx-star"></i>
-        </div>
-        <div class="price"><?php echo $row ["price"];?></div>
-      </div>
+<div class="products">
+<table>
+  <thead>
+  <th>ID</th>
+  <th>Category</th>
+  <th>User_Id</th>
+  <th>Title</th>
+  <th>Image</th>
+  <th>Price</th>
+  </thead>
+  <tbody>
+
+  </tbody>
+</table>
 </div>
