64b414a462f81cd291381531f078c14c5f736d30
DiamantElezi <elezidiamant@gmail.com>
2021-02-24 02:10:41 +0100

    details

--- Projekt/Projekt/admin/post.php
+++ Projekt/Projekt/admin/post.php
@@ -2,21 +2,90 @@
 require 'database/dbconfig.php';
 include 'inc/head.php';
 include 'inc/navbar.php';
-
 ?>
 
 <div class="products">
+<h1>Latest Products</h1>
+<main>
 <table>
   <thead>
+    <tr>
   <th>ID</th>
   <th>Category</th>
   <th>User_Id</th>
   <th>Title</th>
   <th>Image</th>
   <th>Price</th>
+  </tr>
   </thead>
   <tbody>
-
+    <tr>
+      <td>1</td>
+      <td>Technology</td>
+      <td>2</td>
+      <td>Hp Pavilion i5</td>
+      <td><img src="./img/product1.jpg" width="50px"></td>
+      <td>899</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Technology</td>
+      <td>2</td>
+      <td>Security Camera</td>
+      <td><img src="./img/product2.jpg" width="50px"></td>
+      <td>150</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Technology</td>
+      <td>2</td>
+      <td>Smartwatch</td>
+      <td><img src="./img/product3.jpg" width="50px"></td>
+      <td>150</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Technology</td>
+      <td>2</td>
+      <td>Smartphone</td>
+      <td><img src="./img/product4.jpg" width="50px"></td>
+      <td>600</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>Technology</td>
+      <td>2</td>
+      <td>Andorid Smartphone</td>
+      <td><img src="./img/product5.jpg" width="50px"></td>
+      <td>600</td>
+    </tr>
+    <tr>
+      <td>6</td>
+      <td>Technology</td>
+      <td>2</td>
+      <td>Headphones PRO</td>
+      <td><img src="./img/product6.jpg" width="50px"></td>
+      <td>200</td>
+    </tr>
+    <tr>
+      <td>7</td>
+      <td>Technology</td>
+      <td>2</td>
+      <td>SmartWatch PRO</td>
+      <td><img src="./img/product7.jpg" width="50px"></td>
+      <td>350</td>
+    </tr>
+    <tr>
+      <td>8</td>
+      <td>Technology</td>
+      <td>2</td>
+      <td>Security Camera X</td>
+      <td><img src="./img/product2.jpg" width="50px"></td>
+      <td>200</td>
+    </tr>
   </tbody>
 </table>
+</main>
 </div>
+</body>
+</html>
